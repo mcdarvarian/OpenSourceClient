59fc14108fd92b6b32bfa86bc8c33547212de14e
Jack <pupeljack@gmail.com>
2020-06-19 13:59:12 -0500

    added forms and basic functionality to the views 1 and 2

--- app/app.js
+++ app/app.js
@@ -8,8 +8,52 @@ angular.module('myApp', [
   'myApp.view3',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider.otherwise({ redirectTo: '/view1' });
+  }]);
+
+let todoArray = {
+  name: '',
+  sport: '',
+  team: '',
+  gender: '',
+  married: false
+};
+
+function page1Submit() {
+  todoArray = {
+    name:  document.getElementById('name').value,
+    sport: '',
+    team: '',
+    gender: '',
+    married: false
+  };
+  console.log(todoArray);
+}
+function page2Submit(values) {
+  todoArray = {
+    name: todoArray.name,
+    sport: document.getElementById('sport').value,
+    team: document.getElementById('team').value,
+    gender: '',
+    married: false
+  };
+  console.log(todoArray);
+}
+function page3Submit(values) {
+  console.log(document.getElementById('todoForm').value)
+  todoArray = {
+    name: todoArray.name,
+    sport: todoArray.sport,
+    team: todoArray.team,
+    gender: document.getElementById('gender').value,
+    married: document.getElementsByName('marriage').value
+  };
+  console.log('todo is', todoArray);
+}
+
+function check() {
+  console.log('check is', todoArray);
+}
